110e198537ef649cade1165781c8c474b6086cad
Brice Lenfant <165644+bricel@users.noreply.github.com>
2026-01-10 11:40:01 +0200

    new stable app

---
+++ stable/user-ui/static/js/6.4f75c77f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcircuit_user_ui=self.webpackChunkcircuit_user_ui||[]).push([[6],{9006(u,c,e){e.r(c),e.d(c,{default:()=>s});const s={}}}]);
+//# sourceMappingURL=6.4f75c77f.chunk.js.map
